8f49c4a38034c003105afcdb9ffa273eb7a17a48
Jonathan Beckman <jbecks.media@gmail.com>
2017-10-11 14:18:21 -0600

    finish building out posts and gets

--- db/seeds/dev/ports.js
+++ db/seeds/dev/ports.js
@@ -53,6 +53,7 @@ const createPort = (knex, port) => {
 
     return Promise.all([portPromise, ...shipPromises]);
   })
+  .catch(error => console.error('Error seeding data', error))
 };
 
 exports.seed = (knex, Promise) => {
