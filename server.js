e84824043d35159602965025d261e54297a70fd4
Margo Schaedel <margoschaedel@gmail.com>
2017-10-10 18:23:48 -0600

    Write port usage endpoint

--- server.js
+++ server.js
@@ -20,14 +20,39 @@ app.listen(app.get('port'), () => {
 
 // ENDPOINTS
 
-app.get('/api/v1/projects', (request, response) => {
-  database('projects').select()
-    .then( projects => response.status(200).json(projects) )
+app.get('/api/v1/ports', (request, response) => {
+  database('ports').select()
+    .then( ports => response.status(200).json(ports) )
     .catch( error => response.status(500).json({ error }))
 });
 
-app.get('/api/v1/palettes', (request, response) => {
-  database('palettes').select()
-    .then( palettes => response.status(200).json(palettes))
+app.get('/api/v1/port-usage', (request, response) => {
+  database('port_usage').select()
+    .then( ports => response.status(200).json(ports) )
+    .catch( error => response.status(500).json({ error }))
+});
+
+app.get('/api/v1/ships', (request, response) => {
+  database('ships').select()
+    .then( ships => response.status(200).json(ships))
+    .catch( error => response.status(500).json({ error }))
+});
+
+app.get('/api/v1/ports/:id', (request, response) => {
+  const { id } = request.params
+  database('ports').where({ id }).select()
+    .then( port => {
+      let portUsage = database('port_usage').where({ id }).select()
+      const result = Object.assign({}, port[0], { port_usage: portUsage[0] })
+      return result
+    })
+    .then( result => response.status(200).json(result) )
+    .catch( error => response.status(500).json({ error }))
+});
+
+app.get('/api/v1/ships/:id', (request, response) => {
+  const { id } = request.params
+  database('ships').select()
+    .then( ship => response.status(200).json(ship))
     .catch( error => response.status(500).json({ error }))
 });
